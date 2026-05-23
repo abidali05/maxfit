bc871e6b8ddc57208609c5b4406423f387ea201c
Abid Ali <80045928+abidali05@users.noreply.github.com>
2026-05-23 16:35:03 +0500

    changes

--- routes/api.php
+++ routes/api.php
@@ -70,7 +70,6 @@ Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('delete-account', [AuthController::class, 'deleteAccount']);
 
-
     Route::get('/profile', [AuthController::class, 'profile']);
     Route::post('/update-info', [PersonalInfoController::class, 'updateInfo']);
     Route::post('/user-personal-info', [PersonalInfoController::class, 'profile']);
